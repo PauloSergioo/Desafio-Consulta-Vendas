b5e4a32ad853f262cc9e8f2d1b67c9af514150c0
Paulo Sergio <paulosergioilha38@gmail.com>
2023-03-06 12:29:44 -0300

    SQL Changes

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -18,15 +18,15 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
     @Query(nativeQuery = true, value = "SELECT tb_seller.name, SUM(amount) AS amount " +
             "FROM tb_sales  " +
             "INNER JOIN tb_seller  ON tb_seller.id = tb_sales.seller_id " +
-            "WHERE date BETWEEN :min AND :max " +
+            "WHERE date BETWEEN :minDate AND :maxDate " +
             "GROUP BY tb_seller.name")
-    List<SaleMinProjection> searchSaleSummary(LocalDate min, LocalDate max);
+    List<SaleMinProjection> searchSaleSummary(LocalDate minDate, LocalDate maxDate);
 
     @Query(nativeQuery = true, value = "SELECT tb_sales.date, tb_sales.amount, tb_seller.name " +
             "FROM tb_sales " +
             "INNER JOIN tb_seller ON tb_seller.id = tb_sales.seller_id " +
-            "WHERE date BETWEEN :min AND :max AND UPPER(tb_seller.name)  LIKE UPPER(CONCAT('%', :name, '%'))")
-    Page<SellerMinProjection> searchSellerReport(LocalDate min, LocalDate max, String name, Pageable pageable);
+            "WHERE date BETWEEN :minDate AND :maxDate AND UPPER(tb_seller.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<SellerMinProjection> searchSellerReport(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable);
 }
 
 
